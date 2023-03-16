e19ab4a7e876ee7aad07dcc3a765ee2c8d16d750
Viacheslav <gitcoy@outlook.com>
2023-03-17 03:11:57 +0700

    FIX: panic if scrolled to the beginning

--- src/app/binflowviewer/window/components/chart/mod.rs
+++ src/app/binflowviewer/window/components/chart/mod.rs
@@ -39,6 +39,12 @@ glib::wrapper! {
         @extends Widget, DrawingArea;
 }
 
+const PADDING_LEFT: f64 = 20.0;
+const PADDING_RIGHT: f64 = 80.0;
+const PADDING_BOTTOM: f64 = 20.0;
+const PADDING_TOP: f64 = 20.0;
+const PADDING_CHART: f64 = 20.0; // отступ графика от краёв системы координат
+
 impl BChartComponent {
 
     pub fn set_values(&self, values: Vec<f64>) {
@@ -71,8 +77,8 @@ impl BChartComponent {
                 if new_offset < 0 {
                     new_offset = 0;
                 }
-                if new_offset as usize > this.imp().values.borrow().len() {
-                    new_offset = this.imp().values.borrow().len() as i32;
+                if new_offset as usize >= this.imp().values.borrow().len() {
+                    new_offset = this.imp().values.borrow().len() as i32 - 1;
                 }
                 this.imp().offset.replace(new_offset as usize);
                 this.queue_draw();
@@ -86,73 +92,67 @@ impl BChartComponent {
     }
 
     pub fn draw_grid(&self, _drawing_area: &DrawingArea, ctx: &Context, width: i32, height: i32) {
-        const PADDING_LEFT: f64 = 20.0;
-        const PADDING_RIGHT: f64 = 80.0;
-        const PADDING_BOTTOM: f64 = 20.0;
-        const PADDING_TOP: f64 = 20.0;
-        const PADDING_CHART: f64 = 20.0; // отступ графика от краёв системы координат
-
         // Фон
         ctx.set_source_rgb(255.0 / 255.0, 255.0 / 255.0, 255.0 / 255.0); // Background color
         ctx.paint().expect("Error in: components.chart.draw_grid > ctx.paint()");
 
         // Отображение шкалы и сетки на основе загруженных данных
-
-        // Оси
-        ctx.set_source_rgb(0.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0);
-        ctx.set_line_width(1.0);
-        ctx.move_to(PADDING_LEFT, height as f64 - PADDING_BOTTOM);
-        ctx.line_to(width as f64 - PADDING_RIGHT, height as f64 - PADDING_BOTTOM); // Ось абсцисс
-        ctx.line_to(width as f64 - PADDING_RIGHT, PADDING_TOP); // Ось ординат
-        ctx.stroke().expect("Error in: components.chart.draw_grid > ctx.stroke()");
-
-        // Шкала оси абсцисс и вертикальная разметка сетки
-        let dt = Local::now();
-        let dh = dt.hour();
-        let dm = dt.minute();
-        // let mdt = dt - Duration::seconds(dt.second() as i64);
-
-        let timeframe = 1; // minutes
-        let m = 60 * timeframe;
-
-        // Вычисление требуемого для сетки кол-ва вертикальных линий
-        let mut cx = 1 + (width - PADDING_LEFT as i32 - PADDING_RIGHT as i32) / m;
-        if (width - PADDING_LEFT as i32 - PADDING_RIGHT as i32) % m > dm as i32 {
-            cx = cx + 1;
-        }
-
-        // Отображение вертикальных линий сетки
-        for n in 1 .. cx {
-            let x = (width as f64 - PADDING_RIGHT) - dm as f64 - ((n - 1) * m) as f64;
-            ctx.set_source_rgb(0.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0);
-            ctx.move_to(x, height as f64 - PADDING_BOTTOM);
-            ctx.line_to(x, height as f64 - (PADDING_BOTTOM + 4.0));
-            ctx.stroke();
-
-            // Вертикальная разметка сетки
-            ctx.set_source_rgb(230.0 / 255.0, 230.0 / 255.0, 230.0 / 255.0);
-            ctx.move_to(x, height as f64 - (PADDING_BOTTOM + 5.0));
-            ctx.line_to(x, PADDING_TOP);
-            ctx.stroke();
-
-            // Подпись (время)
-            let mut t = dh as i32 + 1 - n;
-            while t < 0 {
-                t = t + 24;
-            }
-            let sh = t.to_string();
-            ctx.set_source_rgb(17.0 / 255.0, 34.0 / 255.0, 45.0 / 255.0);
-            ctx.set_font_size(14.0);
-            let b = ctx.text_extents(sh.as_str()).unwrap();
-            ctx.move_to(x - (b.width / 2.0 + b.x_bearing), height as f64 - 4.0);
-            ctx.show_text(sh.as_str());
-        }
-
-        let values = self.imp().values.borrow();
-        let offset = self.imp().offset.borrow().clone();
         // TODO: показывать графики с длиной меньше width
-        if values.len() > width as usize {
-            let v = &values[(values.len() - width as usize + PADDING_LEFT as usize + PADDING_RIGHT as usize) - offset..values.len() - offset];
+        let values = self.imp().values.borrow();
+        if values.len() > 0 {
+            // Оси
+            ctx.set_source_rgb(0.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0);
+            ctx.set_line_width(1.0);
+            ctx.move_to(PADDING_LEFT, height as f64 - PADDING_BOTTOM);
+            ctx.line_to(width as f64 - PADDING_RIGHT, height as f64 - PADDING_BOTTOM); // Ось абсцисс
+            ctx.line_to(width as f64 - PADDING_RIGHT, PADDING_TOP); // Ось ординат
+            ctx.stroke().expect("Error in: components.chart.draw_grid > ctx.stroke()");
+
+            // Шкала оси абсцисс и вертикальная разметка сетки
+            let dt = Local::now();
+            let dh = dt.hour();
+            let dm = dt.minute();
+            // let mdt = dt - Duration::seconds(dt.second() as i64);
+
+            let timeframe = 1; // minutes
+            let m = 60 * timeframe;
+
+            // Вычисление требуемого для сетки кол-ва вертикальных линий
+            let mut cx = 1 + (width - PADDING_LEFT as i32 - PADDING_RIGHT as i32) / m;
+            if (width - PADDING_LEFT as i32 - PADDING_RIGHT as i32) % m > dm as i32 {
+                cx = cx + 1;
+            }
+
+            // Отображение вертикальных линий сетки
+            for n in 1 .. cx {
+                let x = (width as f64 - PADDING_RIGHT) - dm as f64 - ((n - 1) * m) as f64;
+                ctx.set_source_rgb(0.0 / 255.0, 0.0 / 255.0, 0.0 / 255.0);
+                ctx.move_to(x, height as f64 - PADDING_BOTTOM);
+                ctx.line_to(x, height as f64 - (PADDING_BOTTOM + 4.0));
+                ctx.stroke();
+
+                // Вертикальная разметка сетки
+                ctx.set_source_rgb(230.0 / 255.0, 230.0 / 255.0, 230.0 / 255.0);
+                ctx.move_to(x, height as f64 - (PADDING_BOTTOM + 5.0));
+                ctx.line_to(x, PADDING_TOP);
+                ctx.stroke();
+
+                // Подпись (время)
+                let mut t = dh as i32 + 1 - n;
+                while t < 0 {
+                    t = t + 24;
+                }
+                let sh = t.to_string();
+                ctx.set_source_rgb(17.0 / 255.0, 34.0 / 255.0, 45.0 / 255.0);
+                ctx.set_font_size(14.0);
+                let b = ctx.text_extents(sh.as_str()).unwrap();
+                ctx.move_to(x - (b.width / 2.0 + b.x_bearing), height as f64 - 4.0);
+                ctx.show_text(sh.as_str());
+            }
+
+            let offset = self.imp().offset.borrow().clone();
+            let from = std::cmp::max(0, (values.len() as i32 - width + PADDING_LEFT as i32 + PADDING_RIGHT as i32) - offset as i32) as usize;
+            let v = &values[from..values.len() - offset];
             let max = v.iter().copied().fold(f64::NEG_INFINITY, f64::max);
             let min = v.iter().copied().fold(f64::INFINITY, f64::min);
 
@@ -184,18 +184,13 @@ impl BChartComponent {
     }
 
     pub fn draw_chart(&self, _drawing_area: &DrawingArea, ctx: &Context, width: i32, height: i32) {
-        const PADDING_LEFT: f64 = 20.0;
-        const PADDING_RIGHT: f64 = 80.0;
-        const PADDING_BOTTOM: f64 = 20.0;
-        const PADDING_TOP: f64 = 20.0;
-        const PADDING_CHART: f64 = 20.0; // отступ графика от краёв системы координат
-
         // let x = self.imp().values.borrow_mut();
-        let values = self.imp().values.borrow();
-        let offset = self.imp().offset.borrow().clone();
         // TODO: показывать графики с длиной меньше width
-        if values.len() > width as usize {
-            let v = &values[(values.len() - width as usize + PADDING_LEFT as usize + PADDING_RIGHT as usize) - offset..values.len() - offset];
+        let values = self.imp().values.borrow();
+        if values.len() > 0 {
+            let offset = self.imp().offset.borrow().clone();
+            let from = std::cmp::max(0, (values.len() as i32 - width + PADDING_LEFT as i32 + PADDING_RIGHT as i32) - offset as i32) as usize;
+            let v = &values[from..values.len() - offset];
             let max = v.iter().copied().fold(f64::NEG_INFINITY, f64::max);
             let min = v.iter().copied().fold(f64::INFINITY, f64::min);
 
